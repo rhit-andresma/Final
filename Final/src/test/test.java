a98d323275686e52ef78d62f3ea51dbe71f7ffff
koglerac <koglerac@rose-hulman.edu>
2025-10-30 15:43:46 -0400

    nothing

--- Final/src/test/test.java
+++ Final/src/test/test.java
@@ -1,10 +1,18 @@
 package test;
+
+import javax.swing.SwingUtilities;
+
 //what is going on
 public class test {
     //I have no idea. Does this work?
 	//how about this second test?
 	//3rd test
 	//4th test
-	
+	//5th test
+public static void main(String[] args) {
+		
+		System.out.println("nothing");
+
+	}
 	
 }
